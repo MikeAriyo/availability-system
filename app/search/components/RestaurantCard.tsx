0f1a00e4b916ccc6b79a866147274b36fa516859
MikeAriyo <ariyoadekola2@gmail.com>
2023-06-16 18:44:31 +0100

    cleaning the search directory

---
+++ app/search/components/RestaurantCard.tsx
@@ -0,0 +1,28 @@
+export default function RestaurantCard() {
+  return (
+    <div className="border-b flex pb-5">
+      <img
+        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
+        alt=""
+        className="w-44 rounded"
+      />
+      <div className="pl-5">
+        <h2 className="text-3xl text-black">Aiāna Restaurant Collective</h2>
+        <div className="flex items-start">
+          <div className="flex mb-2 text-black">*****</div>
+          <p className="ml-2 text-sm text-black">Awesome</p>
+        </div>
+        <div className="mb-9">
+          <div className="font-light flex text-reg">
+            <p className="mr-4 text-black">$$$</p>
+            <p className="mr-4 text-black">Mexican</p>
+            <p className="mr-4 text-black">Ottawa</p>
+          </div>
+        </div>
+        <div className="text-red-600">
+          <a href="">View more information</a>
+        </div>
+      </div>
+    </div>
+  );
+}
