c3ef894940c0407791ef89cf0dbb24cdac403395
MikeAriyo <ariyoadekola2@gmail.com>
2023-06-19 02:09:08 +0100

    defining page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import NavBar from "./components/NavBar";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <main className="bg-gray-100 min-h-screen w-screen">
+          <main className="max-w-screen-2xl m-auto bg-white">
+            <NavBar />
+            {children}
+          </main>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,11 @@ import RestaurantCard from "./components/RestaurantCard";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <main>
-          <Header />
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
-          </div>
-        </main>
-      </main>
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        <RestaurantCard />
+      </div>
     </main>
   );
 }
