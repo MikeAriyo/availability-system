68817b706c61e2cbeb31e877b8e5a4266ad73bc1
MikeAriyo <ariyoadekola2@gmail.com>
2023-06-19 01:32:59 +0100

    fixed import issue for next router

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
   const [location, setLocation] = useState("");
