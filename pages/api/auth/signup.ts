6afae2392019d47e69469911e1ef32e32ea4c78b
Adekola Ariyo <ariyo@zebet.ng>
2023-08-06 22:24:42 +0100

    creating an endpoint

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    res.status(200).json({
+      hell0: "there",
+    });
+  }
+}
