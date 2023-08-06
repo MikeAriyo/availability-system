cb8819f5c11328d3a3f6769e848de759287d045e
Adekola Ariyo <ariyo@zebet.ng>
2023-08-06 22:54:18 +0100

    validating users input

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,10 +1,66 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
+    const body = req.body;
+    const { firstName, lastName, email, phone, city, password } = req.body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+
+        errorMessage: "First name is Invalid",
+      },
+
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+
+        errorMessage: "Last name is Invalid",
+      },
+
+      {
+        valid: validator.isEmail(email),
+
+        errorMessage: "Email is Invalid",
+      },
+
+      {
+        valid: validator.isMobilePhone(phone),
+
+        errorMessage: "Phone number is invalid",
+      },
+
+      {
+        valid: validator.isLength(city, { min: 1 }),
+
+        errorMessage: "City is Invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is invalid",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
     res.status(200).json({
       hell0: "there",
     });
