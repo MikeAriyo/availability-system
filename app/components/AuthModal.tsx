ecead0a50637a7d4fe60100288ba5e05aa6ad1be
Adekola Ariyo <ariyo@zebet.ng>
2023-08-08 10:36:44 +0100

    added client side validation on inputs

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AuthModalInputs from "./AuthModalInputs";
 
 const style = {
@@ -23,6 +23,7 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [disabled, setDisabled] = useState(false);
 
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignin ? signinContent : signupContent;
@@ -43,6 +44,27 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
     city: "",
     password: "",
   });
+
+  useEffect(() => {
+    if (isSignin) {
+      if (!inputs.password && inputs.email) {
+        return setDisabled(false);
+      }
+    } else {
+      if (
+        inputs.firstName &&
+        inputs.lastName &&
+        inputs.email &&
+        inputs.password &&
+        inputs.city &&
+        inputs.phone
+      ) {
+        return setDisabled(false);
+      }
+    }
+
+    setDisabled(true);
+  }, [inputs]);
 
   return (
     <div>
@@ -79,10 +101,13 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
               <AuthModalInputs
                 inputs={inputs}
                 handleChangeInput={handleChangeInput}
-                isSignin = {isSignin}
+                isSignin={isSignin}
               />
 
-              <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+              <button
+                className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"
+                disabled={disabled}
+              >
                 {renderContent("Sign In", "Create Account")}
               </button>
             </div>
