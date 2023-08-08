6c0c278ce1be0768b5019fb61e4fa80f23bd07df
Adekola Ariyo <ariyo@zebet.ng>
2023-08-08 10:46:22 +0100

    installed axios

---
+++ hooks/useAuth.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const useAuth = () => {
+  const signin = async () => {};
+  const signup = async () => {};
+
+  return {
+    signin,
+    signup,
+  };
+};
+
+export default useAuth;
